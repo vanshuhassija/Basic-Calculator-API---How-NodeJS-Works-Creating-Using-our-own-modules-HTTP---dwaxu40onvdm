791b277b8cce2ee1cc21dc70cfcd0b08bebaa310
vanshuhassija <33016642+vanshuhassija@users.noreply.github.com>
2023-02-27 21:56:04 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const bodyParser = require("body-parser");
+const port = 3000
+app.use(express.urlencoded());
+
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(bodyParser.json())
+// your code goes here
+
+
+app.listen(port, () => console.log(`App listening on port ${port}!`))
+
+module.exports = app;
